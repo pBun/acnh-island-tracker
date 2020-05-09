19ad6ba5829762eddf2b32b7032068f281e1ad3c
Dustin Boersma <dboersma@blizzard.com>
2020-05-09 04:39:37 -0700

    added villager icon hook

---
+++ src/hooks/useVillagerIcons.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+
+export default function useVillagerIcons() {
+    const villagerIconsQuery = useStaticQuery(
+        graphql`
+            query {
+                allFile(filter: { relativeDirectory: { eq: "villager-icons" } }) {
+                    edges {
+                        node {
+                            publicURL
+                            name
+                        }
+                    }
+                }
+            }
+        `
+    );
+    const villagerIcons = {};
+    villagerIconsQuery.allFile.edges.forEach(edge => { villagerIcons[edge.node.name] = edge.node.publicURL });
+    return villagerIcons;
+}
