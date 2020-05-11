40a8ea2201df65ee3dea06c22e6c2a3b52b6af17
Dustin Boersma <dboersma@blizzard.com>
2020-05-11 00:52:45 -0700

    Updates

---
+++ component---src-pages-data-by-villager-js-2bb225a43725b50d1220.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{L8bj:function(e,a,n){"use strict";n.r(a);var t=n("q1tI"),r=n.n(t),l=n("1lFR"),i=n("YQnL");a.default=function(){return r.a.createElement(i.a,{title:"Grouped by Villager"},r.a.createElement(l.a,{villagerPropName:"name"}))}}}]);
+//# sourceMappingURL=component---src-pages-data-by-villager-js-2bb225a43725b50d1220.js.map
