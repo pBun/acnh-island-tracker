3708e7f7b5f378e9640167427e8551490c35bf87
Dustin Boersma <dboersma@blizzard.com>
2020-05-10 21:51:37 -0700

    removed console.log

--- src/util/dataShare.js
+++ src/util/dataShare.js
@@ -7,7 +7,6 @@ const DATA_UPLOAD_URL =
 
 export function shareSighting({ id, villager, timestamp, islandOffset }) {
     return new Promise((resolve, reject) => {
-        console.log(id, villager, timestamp, islandOffset);
         const qs = queryString.stringify({
             timestamp: encodeURIComponent(
                 format(timestamp, "MM/dd/yyyy hh:mm:ss")
