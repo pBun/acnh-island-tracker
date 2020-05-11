24fcaa24b0e34c848fcbdaa24a21383bde0c05ce
Dustin Boersma <dboersma@blizzard.com>
2020-05-11 12:29:59 -0700

    Updates

---
+++ component---src-pages-data-mystery-islands-by-species-js-b4ffdee0acd02bffe010.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{zi55:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),i=a.n(n),r=a("1lFR"),c=a("uVCN");t.default=function(){return i.a.createElement(c.a,{title:"Grouped by Species"},i.a.createElement(r.a,{villagerPropName:"species"}))}}}]);
+//# sourceMappingURL=component---src-pages-data-mystery-islands-by-species-js-b4ffdee0acd02bffe010.js.map
