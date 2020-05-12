250fe6845018915a50c5a86f9c1b4c8f3c7f5a91
Dustin Boersma <dboersma@blizzard.com>
2020-05-12 14:53:53 -0700

    fixed percents for mi

--- src/hooks/useVillagerCalc.js
+++ src/hooks/useVillagerCalc.js
@@ -5,13 +5,13 @@ export default function useVillagerCalc(props) {
     // const { currentResidents, pastResidents } = props || {};
     const { allVillagers, getVillager } = useVillagers();
     const availableSpecies = groupBy(allVillagers, 'species');
-    const availablePersonalities = groupBy(allVillagers, 'personality');
+    // const availablePersonalities = groupBy(allVillagers, 'personality');
     const getMysteryIslandRate = villagerName => {
         const villager = getVillager(villagerName);
         if (!villager) return 0;
-        const numSpecies = villager.species && availableSpecies[villager.species] ? availableSpecies[villager.species].length : 0;
-        const numPersonalities = villager.personality && availablePersonalities[villager.personality] ? availablePersonalities[villager.personality].length : 0;
-        return Math.round((1 / numPersonalities)*(1 / numSpecies) * 10000) / 100;
+        const numTotalSpecies = availableSpecies && Object.keys(availableSpecies).length;
+        const numSpeciesVillagers = villager.species && availableSpecies[villager.species] ? availableSpecies[villager.species].length : 0;
+        return Math.round((1 / numTotalSpecies)*(1 / numSpeciesVillagers) * 10000) / 100;
     };
     const getCampsiteRate = villagerName => {
         return 0;
