702241ee6a88123a7f01959e147579de23f411e9
Dustin Boersma <dboersma@blizzard.com>
2020-05-13 01:05:51 -0700

    fixed 404 styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,21 @@
 import React from "react";
-
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 import Page from "../components/Page";
-
+const useStyles = makeStyles(theme => ({
+    text: {
+        padding: theme.spacing(3, 5, 5),
+    },
+}));
 const NotFoundPage = () => {
+    const classes = useStyles();
     return (
         <Page title="404: Not Found">
-            ...Well there you have it. According to recent feedback, the general sentiment is that
-            this page was not found. Well I think there's only one way to respond to this
-            information... Try one of the other pages in the menu. And there you have it!
+            <Typography variant="body1" className={classes.text}>
+                ...Well there you have it. According to recent feedback, the general sentiment is that
+                this page was not found. Well I think there's only one way to respond to this
+                information... Try one of the other pages in the menu. And there you have it!
+            </Typography>
         </Page>
     );
 };
